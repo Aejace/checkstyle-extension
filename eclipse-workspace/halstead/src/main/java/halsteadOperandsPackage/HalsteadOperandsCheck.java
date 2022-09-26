48eed14e07d9545ab5f34fa2ac911ff3f1e48acc
Arlo Jones <Arloe.jones3@gmail.com>
2022-09-25 21:58:23 -0700

    Updated Operators and operands list

--- eclipse-workspace/halstead/src/main/java/halsteadOperandsPackage/HalsteadOperandsCheck.java
+++ eclipse-workspace/halstead/src/main/java/halsteadOperandsPackage/HalsteadOperandsCheck.java
@@ -8,20 +8,19 @@ public class HalsteadOperandsCheck extends AbstractCheck {
 	private String message = "Operands Count: ";
 	
 	int[] tokens = new int[] {TokenTypes.CHAR_LITERAL,
+			TokenTypes.EMPTY_STAT,
+			TokenTypes.EXPR,
+			TokenTypes.FOR_CONDITION,
+			TokenTypes.FOR_INIT,
+			TokenTypes.FOR_ITERATOR,
 			TokenTypes.IDENT,
-			TokenTypes.LITERAL_BOOLEAN,
-			TokenTypes.LITERAL_BYTE,
-			TokenTypes.LITERAL_CHAR,
-			TokenTypes.LITERAL_DOUBLE,
-			TokenTypes.LITERAL_FLOAT,
-			TokenTypes.LITERAL_INT,
-			TokenTypes.LITERAL_LONG,
-			TokenTypes.LITERAL_SHORT,
-			TokenTypes.LITERAL_VOID,
+			TokenTypes.LABELED_STAT,
+			TokenTypes.METHOD_REF,
 			TokenTypes.NUM_DOUBLE,
 			TokenTypes.NUM_FLOAT,
 			TokenTypes.NUM_INT,
 			TokenTypes.NUM_LONG,
+			TokenTypes.RESOURCE,
 			TokenTypes.STRING_LITERAL};
 	
 	@Override
